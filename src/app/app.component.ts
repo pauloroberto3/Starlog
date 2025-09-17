59f039ce0f6b96e1464b6146522f9a6762e7f8eb
pauloroberto3 <paulormds412@gmail.com>
2025-09-17 14:08:28 -0300

    Adicinando rotas e criando cadastro component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
-import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
