206d6ffe4a98675deb4e5c69a2548c7c712345c7
pauloroberto3 <paulormds412@gmail.com>
2025-09-17 15:13:48 -0300

    Adicionando hover na header, login e cadastro

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
+  imports: [RouterLink]
 })
 export class CadastroComponent implements OnInit {
 
