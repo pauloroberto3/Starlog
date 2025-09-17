d1fb74e17bf5229c9d99c68c5283131eeff666e8
pauloroberto3 <paulormds412@gmail.com>
2025-09-17 14:17:22 -0300

    Adicinando routerlink no login e na header

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  imports: [RouterLink]
 })
 export class LoginComponent implements OnInit {
 
